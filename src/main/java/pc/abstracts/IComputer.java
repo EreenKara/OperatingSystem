a4c72f1df16ef7fa579238b9e163276ee0e5b0a6
Emirhan ETLİ <emirhanetli2003@gmail.com>
2023-12-30 23:52:01 +0300

    added computer logics

--- src/main/java/pc/abstracts/IComputer.java
+++ src/main/java/pc/abstracts/IComputer.java
@@ -1,10 +1,13 @@
 package main.java.pc.abstracts;
 
+import main.java.iodevices.abstracts.IModem;
+import main.java.os.abstracts.IOS;
+
 public interface IComputer {
     void turnOn();
     void turnOff();
-
-
+    IOS getOS();
+    boolean loadOperatingSystem();
 
 
 
--- src/main/java/pc/impl/Computer.java
+++ src/main/java/pc/impl/Computer.java
@@ -1,12 +1,33 @@
 package main.java.pc.impl;
 
+import main.java.iodevices.abstracts.IModem;
+import main.java.iodevices.abstracts.IPrinter;
+import main.java.iodevices.abstracts.IScanner;
+import main.java.os.abstracts.IOS;
+import main.java.os.impl.OS;
+import main.java.pc.abstracts.ICPU;
 import main.java.pc.abstracts.IComputer;
+import main.java.pc.abstracts.IDisplay;
+import main.java.pc.abstracts.IRAM;
+
+import java.util.List;
 
 public class Computer implements IComputer {
     boolean isOn;
 
+    private IOS operatingSystem;
+    private IRAM ram;
+    private List<IPrinter> printers;
+    private List<IScanner> scanners;
+    private List<IModem> modems;
+    private IDisplay display;
+    private ICPU cpu;
+
     public Computer() {
         isOn=false;
+        this.operatingSystem = new OS();
+        this.cpu = new CPU();
+        this.display = new Display();
     }
 
     @Override
@@ -18,4 +39,16 @@ public class Computer implements IComputer {
     public void turnOff() {
         isOn=false;
     }
+
+    @Override
+    public IOS getOS() {
+        return this.operatingSystem;
+    }
+
+    @Override
+    public boolean loadOperatingSystem() {
+        return false;
+    }
+
+
 }
